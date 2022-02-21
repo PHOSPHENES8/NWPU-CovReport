f5929f2684d6e198942505f7788d3a59dbd3ed26
hiyoungshen <shen253135371@163.com>
2022-02-21 14:23:45 +0800

    增加对linux支持，mac不知道行不行，按照linux处理

--- README.md
+++ README.md
@@ -9,13 +9,83 @@
     pip install -r requirements.txt
     ```
 2. 开启stmp服务：不同邮箱开启方式不同，可自行百度
-   
-3. 修改配置文件
-   
-   配置文件有两个.
-   
-   一个是放在`C:\Users\您的用户名称\infoconf\`文件夹下的`conf.ini`文件(没有自行创建)：
-   > 添加两个section
+
+## Windows用户
+
+1. 配置
+
+    修改配置文件, 配置文件有两个.
+      
+    一个是放在`C:\Users\您的用户名称\.infoconf\`文件夹下的`conf.ini`文件(没有自行创建)：
+
+    > 添加两个section
+    ```ini
+    [info]
+    stu_number=你的翱翔门户学号
+    stu_passwd=你的翱翔门户密码
+    url = http://yqtb.nwpu.edu.cn/wx/xg/yz-mobile/index.jsp
+
+    [163email]
+    from_addr = 你的163邮箱
+    password  = 你的163 smtp/pop3 密码
+    to_addr = 你要给哪个邮箱发邮件
+    smtp_server = smtp.163.com
+    ```
+    > 例如改成如下:
+    ```ini
+    [info]
+    stu_number=8888888888
+    stu_passwd=cym8888888
+    url = http://yqtb.nwpu.edu.cn/wx/xg/yz-mobile/index.jsp
+
+    [163email]
+    from_addr = 8888888888@163.com
+    password  = TSAAUDYNABCDEFGH
+    to_addr = 8888888888@foxmail.com
+    smtp_server = smtp.163.com
+    ```
+
+    一个是放在`C:\Users\您的用户名称\.webdriverconf\`文件夹下的`conf.ini`文件(没有自行创建)：
+
+    > 添加一个section
+
+    ```ini
+    [driver]
+    absPath=你希望把chromedriver存在哪里，写绝对路径
+    ; url=http://npm.taobao.org/mirrors/chromedriver
+    url=https://chromedriver.storage.googleapis.com
+    ```
+
+    > 例如如下:
+
+    ```ini
+    [driver]
+    absPath=D:/Source/chromedriver/chromedriver.exe
+    ; url=http://npm.taobao.org/mirrors/chromedriver
+    url=https://chromedriver.storage.googleapis.com
+    ```
+
+    **注意：absPath=D:/Source/chromedriver/chromedriver.exe，最后的chromedriver.exe要加上，代表chromedriver.exe这个文件**
+
+2. 打包
+
+* 因为使用到了本地的python类包，和相对路径有关，故需要打包。
+* pyinstaller -F run.py
+* 文件夹dist下就是可执行exe
+
+3. 定时执行
+
+- 通过控制面板→计划任务→创建基本任务→填写相应内容→完成
+
+## Linux用户
+
+1. 配置
+
+    修改配置文件, 配置文件有两个.
+      
+    一个是放在`/home/您的用户名称/.infoconf/`文件夹下的`conf.ini`文件(没有自行创建)：
+
+    > 添加两个section
     ```ini
     [info]
     stu_number=你的翱翔门户学号
@@ -42,7 +112,8 @@
     smtp_server = smtp.163.com
     ```
 
-    一个是放在`C:\Users\您的用户名称\webdriverconf\`文件夹下的`conf.ini`文件(没有自行创建)：
+    一个是放在`/home/您的用户名称/.webdriverconf/`文件夹下的`conf.ini`文件(没有自行创建)：
+
     > 添加一个section
     ```ini
     [driver]
@@ -50,23 +121,32 @@
     ; url=http://npm.taobao.org/mirrors/chromedriver
     url=https://chromedriver.storage.googleapis.com
     ```
+
     > 例如如下:
+
     ```ini
     [driver]
-    absPath=D:/Source/chromedriver/chromedriver.exe
+    absPath=/home/hiyoungshen/packege/chromedriver
     ; url=http://npm.taobao.org/mirrors/chromedriver
     url=https://chromedriver.storage.googleapis.com
     ```
 
-4. 打包
+    **注意：absPath=D:/Source/chromedriver/chromedriver，最后的chromedriver要加上，代表chromedriver这个文件**
+
+2. 打包(不一定需要)
 
 * 因为使用到了本地的python类包，和相对路径有关，故需要打包。
 * pyinstaller -F run.py
-* 文件夹dist下就是可执行exe
+* 文件夹dist下就是可执行文件
 
-5. 定时执行
+3. 定时执行
 
-- 通过控制面板→计划任务→创建基本任务→填写相应内容→完成
+* linux脚本定时执行例如
+  ```shell
+  sudo crontab -e 进入编辑模式
+  增加一行:
+  59 11 1 * * /home/hiyoungshen/source/NWPU-CovReport/run.sh
+  ```
 
 ## Notes
 
--- run.py
+++ run.py
@@ -4,41 +4,88 @@ from classes.SendEmail import SendEmail
 from classes.ReportingOfEpidemicSituation import ReportingOfEpidemicSituation
 import configparser
 import os
+import platform
 
 if __name__ == "__main__":
     # 读取配置
-    cur_home_dir = "C:" + os.environ["HOMEPATH"]
-    infoconf_dir = os.path.join(cur_home_dir, "infoconf")
-    webdriverconf_dir = os.path.join(cur_home_dir, "webdriverconf")
-    if not os.path.exists(infoconf_dir):
-        os.mkdir(infoconf_dir)
-    if not os.path.exists(webdriverconf_dir):
-        os.mkdir(webdriverconf_dir)
-    
-    infoconf_path = os.path.abspath(os.path.join(infoconf_dir, "conf.ini"))
-    webdriverconf_path = os.path.abspath(os.path.join(webdriverconf_dir, "conf.ini"))
+    cur_os = "Windows"
+    if(platform.system()=='Windows'):
+        cur_os = "Windows"
+        try:
+            cur_home_dir = "C:" + os.environ["HOMEPATH"]
+        except KeyError as e:
+            print(e)
+            exit('找不到os.environ["HOMEPATH"]')
+    elif(platform.system()=='Linux'):
+        cur_os = "Linux"
+        try:
+            cur_home_dir = os.environ["HOME"]
+        except KeyError as e:
+            print(e)
+            exit('找不到os.environ["HOME"]')
+    else:
+        cur_os = "Linux"
+        try:
+            cur_home_dir = os.environ["HOME"]
+        except KeyError as e:
+            print(e)
+            exit('找不到os.environ["HOME"]')
 
-    if not os.path.exists(infoconf_path):
-        print("请现在您windows的家目录下创建 infoconf/conf.ini 文件并正确配置")
-        exit(0)
+    cur_home_dir = os.path.abspath(cur_home_dir) 
+    print("当前配置文件所在文件夹为: {}.".format(cur_home_dir))
+
+    studentinfoconf_dir = os.path.abspath(os.path.join(cur_home_dir, ".studentinfoconf"))
+    webdriverconf_dir = os.path.abspath(os.path.join(cur_home_dir, ".webdriverconf"))
+
+    file_name = os.path.join(studentinfoconf_dir, "conf.ini")
+    if not os.path.exists(file_name):
+        if not os.path.exists(studentinfoconf_dir):
+            os.mkdir(studentinfoconf_dir)
+        with open(file_name,'w',encoding='utf-8'):
+            pass
+        print("请先在您的家目录下创建 .infoconf/conf.ini 文件并正确配置")
+        exit(1)
     
-    if not os.path.exists(webdriverconf_dir):
-        print("请现在您windows的家目录下创建 webdriverconf/conf.ini 文件并正确配置")
-        exit(0)
+    file_name = os.path.join(webdriverconf_dir, "conf.ini")
+    if not os.path.exists(file_name):
+        if not os.path.exists(webdriverconf_dir):
+            os.mkdir(webdriverconf_dir)
+        with open(file_name,'w',encoding='utf-8'):
+            pass
+        print("请先在您的家目录下创建 .webdriverconf/conf.ini 文件并正确配置")
+        exit(1)
+    
+    infoconf_path = os.path.abspath(os.path.join(studentinfoconf_dir, "conf.ini"))
+    webdriverconf_path = os.path.abspath(os.path.join(webdriverconf_dir, "conf.ini"))
     
     # read_info_conf = ReadInfoConf(confpath="infoconf/conf.ini")
     read_info_conf = ReadInfoConf(confpath=infoconf_path)
 
-    stu_number = read_info_conf.get('info', 'stu_number')
-    stu_passwd = read_info_conf.get('info', 'stu_passwd')
-    url = read_info_conf.get('info', 'url')
+    try:
+        stu_number = read_info_conf.get('info', 'stu_number')
+        stu_passwd = read_info_conf.get('info', 'stu_passwd')
+        url = read_info_conf.get('info', 'url')
+    except configparser.NoSectionError:
+        print("请先在您的家目录下创建 .webdriverconf/conf.ini 文件并正确配置")
+        exit(1)
 
     # 检查conf.ini中指定的chromedriver版本和chrome版本是否匹配，不匹配则重新下载并解压
     # chrome_webdriver_setup = Chrome("webdriverconf/conf.ini")
-    chrome_webdriver_setup = Chrome(webdriverconf_path)
-    
+    if cur_os == "Windows":
+        chrome_webdriver_setup = Chrome(webdriverconf_path)
+    elif cur_os == "Linux":
+        chrome_webdriver_setup = Chrome(webdriverconf_path)
+        pass
+    else:
+        chrome_webdriver_setup = Chrome(webdriverconf_path)
+        pass
+
     read_info_conf_webdriver = ReadInfoConf(confpath=webdriverconf_path)
-    executable_path = read_info_conf_webdriver.get('driver', 'absPath')
+    try:
+        executable_path = read_info_conf_webdriver.get('driver', 'absPath')
+    except configparser.NoSectionError:
+        print("请先在您的家目录下创建 .webdriverconf/conf.ini 文件并正确配置")
+        exit(1)
 
     # 疫情填报
     reportingOfEpidemicSituation = ReportingOfEpidemicSituation(

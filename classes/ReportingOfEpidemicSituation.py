9728b936fcacdece554846c6549933bf28cf0dc2
hiyoungshen <shen253135371@163.com>
2022-02-21 13:13:14 +0800

    修复bug

--- classes/ReportingOfEpidemicSituation.py
+++ classes/ReportingOfEpidemicSituation.py
@@ -12,6 +12,7 @@ class ReportingOfEpidemicSituation():
         self.options.add_experimental_option(
             'excludeSwitches', ['enable-logging'])
         # self.options.add_argument('--headless')
+        self.options.add_argument("start-maximized")
         self.driver = webdriver.Chrome(options=self.options, executable_path = executable_path)
 
     def run(self):
@@ -31,7 +32,8 @@ class ReportingOfEpidemicSituation():
         self.driver.implicitly_wait(self.time_out)
         self.driver.find_element_by_partial_link_text('提交填报信息').click()
         self.driver.implicitly_wait(self.time_out)
-        self.driver.find_element_by_class_name('co3').click()
+        self.driver.find_element_by_css_selector('#qrxx_div > div.weui-cells.weui-cells_form > div.weui-cells.weui-cells_checkbox > label > div.weui-cell__bd > p').click()
+        # self.driver.find_element_by_class_name('co3').click()
         self.driver.implicitly_wait(self.time_out)
         self.driver.find_element_by_partial_link_text('确认提交').click()
         self.driver.implicitly_wait(self.time_out)
